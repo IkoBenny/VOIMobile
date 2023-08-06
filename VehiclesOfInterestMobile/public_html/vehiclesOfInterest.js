9d66dd862a7fe9afc6911ad358bc345457fc2fa7
Benny Iko <benny.iko.it@gmail.com>
2023-08-06 16:13:31 -0400

    Adds search button to index. This search button calls showTable() on click.
    Future Development Needs
    
    Need to clone VOIShowButton2:
    to call refreshVOI() clone with search capability.
    button should say Search and search for any existing value equal to string, highlighted.

--- VehiclesOfInterestMobile/public_html/vehiclesOfInterest.js
+++ VehiclesOfInterestMobile/public_html/vehiclesOfInterest.js
@@ -78,6 +78,63 @@ function refreshVOI() {
     }; // end of fucntion
 } // end of refreshSales function
 
+function searchVOI() {
+    // Find a <table> element with id="VOITable":
+    var VOITable = document.getElementById("VOITable");
+    var tableRows = VOITable.getElementsByTagName('tr');
+    var rowCount = VOITable.rows.length;
+    alert("get talbe");
+    for (var r = 1; r < rowCount; r++) {
+        VOITable.deleteRow(-1);
+    }
+
+    var request = makeHttpObject();
+    var xmlDoc;
+    var parser;
+    var x, i;
+    var vois, voi;
+    request.open("GET", "http://localhost:8080/VehiclesOfInterestWebServices/webresources/model.vehicleofinterest", true);
+    request.send();
+    request.onreadystatechange = function () {
+
+        if (request.readyState == 4)
+        {
+            alert("data " + request.responseText.toString());
+            parser = new DOMParser();
+            vois = parser.parseFromString(request.responseText.toString(), "text/xml");
+            voi = vois.documentElement.childNodes;
+
+            for (i = 0; i < voi.length; i++)
+            {
+                var row = VOITable.insertRow(-1);
+
+                // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+                var cell5 = row.insertCell(4);
+                var cell6 = row.insertCell(5);
+                var cell7 = row.insertCell(6);
+                var cell8 = row.insertCell(7);
+
+                // Add some text to the new cells:
+                cell1.innerHTML = vois.getElementsByTagName("licensePlate")[i].childNodes[0].nodeValue;
+                cell2.innerHTML = vois.getElementsByTagName("reason")[(i * 2) + 1].childNodes[0].nodeValue;
+                cell3.innerHTML = vois.getElementsByTagName("make")[(i * 4) + 1].childNodes[0].nodeValue;
+                cell4.innerHTML = vois.getElementsByTagName("model")[(i * 2) + 1].childNodes[0].nodeValue;
+                cell5.innerHTML = vois.getElementsByTagName("vehYear")[i].childNodes[0].nodeValue;
+                cell6.innerHTML = vois.getElementsByTagName("color")[i].childNodes[0].nodeValue;
+                cell7.innerHTML = vois.getElementsByTagName("ownersName")[i].childNodes[0].nodeValue;
+                cell8.innerHTML = vois.getElementsByTagName("ownersPhone")[i].childNodes[0].nodeValue;
+            } // end of for loop
+        }  // end of if statement
+        request.close();
+        showTable();
+    }; // end of fucntion
+} // end of refreshSales function
+
+
 function showTable() {
     var mspImg = document.getElementById('logo');
     mspImg.style.height = '200px';
@@ -85,8 +142,9 @@ function showTable() {
     document.getElementById("VOIcontainer").style.display = "block";
     document.getElementById("VehiclesOfInterestdiv").style.display = "block";
     document.getElementById("VOITable").style.display = "block";
-    document.getElementById("VOIShowButton1").style.display = "block";
-    document.getElementById("VOIShowButton2").style.display = "block";
+    document.getElementById("VOIShowButton1").style.display = "inline-block";
+    document.getElementById("VOIShowButton2").style.display = "inline-block";
+    document.getElementById("SearchButton").style.display = "inline-block";
 }
 
 
